8ea9c536a0f078cdafc1d88bc02d401918f2b0f6
Vgssy <danya.knyazkin@gmail.com>
2024-11-28 21:18:20 +0300

    cors origin ip

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    origins "http://176.114.90.118:3000"
 
     resource "*",
       headers: :any,
